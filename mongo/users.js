59030d79140b38fe55a6933f8befcca7208c3edb
assafsauer <as.sauer@gmail.com>
2022-04-23 11:15:17 +0200

    Update users.js

--- mongo/users.js
+++ mongo/users.js
@@ -6,7 +6,6 @@ db.users.insertMany([
     {name: 'user', password: 'password', email: 'user@me.com'},
     {name: 'stan', password: 'bigbrain', email: 'stan@instana.com'},
     {name: 'partner-57', password: 'worktogether', email: 'howdy@partner.com'},
-    {name: 'datadog', password: 'password', email: 'as.sauer@gmail.com.com'}
 ]);
 
 // unique index on the name
